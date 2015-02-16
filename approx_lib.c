99d01676f81348e91f904ccead4558dbac48fa83
neo <ashwincnaik@gatech.edu>
2015-02-16 13:18:21 -0500

    Changes made to approx_lib impl file and README (compilation directions)

--- approx_lib.c
+++ approx_lib.c
@@ -2,15 +2,16 @@
 #include "approx_lib.h"
 
 // Author : Ashwin Naik
-
 // Approximate Malloc function for approximate memory allocation
 void * approx_malloc(size_t size , int error)
 {
-  return (void *) malloc(sizeof(size));
+  printf("Approximating memory allocation\n");
+  return (void *) malloc(size);
 }
 
 // Approximate Free function for approximating the free memory 
 void approx_free(void * ptr)
 {
+  printf("Approximating memory free\n");
   return free(ptr);
 }
